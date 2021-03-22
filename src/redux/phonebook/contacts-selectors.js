32724d430c3793304d519446e4cf51e6c70ae179
Ivan Khailo <jakom333@gmail.com>
2021-03-22 23:30:45 +0200

    add reselect

--- src/redux/phonebook/contacts-selectors.js
+++ src/redux/phonebook/contacts-selectors.js
@@ -1,17 +1,20 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 const getContacts = (state) => state.contacts;
 
 const getLoader = (state) => state.loader;
 
 const getFilter = (state) => state.filter;
 
-const getFilteredContacts = (state) => {
-  const filter = getFilter(state);
-  const contacts = getContacts(state);
-  const normalizedFilter = filter.toLowerCase();
+const getFilteredContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
 
-  return contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-};
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
 
 export { getContacts, getLoader, getFilter, getFilteredContacts };
